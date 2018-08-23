2d6cf3e5639ad508d7633b1a0b51fc0cc6e25c9f
heather bricca <hbricca@KCH-wxcenter-17.valpo.edu>
2018-08-23 11:33:54 -0500

    My first commit

---
+++ temp_conversion.for
@@ -0,0 +1,12 @@
+      PROGRAM temp_conversion
+
+*******************************************
+* Temperature Conversion Program
+* 
+* By: Heather Bricca
+*
+* I have neither given or received, nor have I 
+* tolerated others' use of unauthorized aid. Heather Bricca
+*
+***************************************************
+
