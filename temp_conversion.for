d4e4e71025e4c28949acd1e24ea89674cad9c914
heather bricca <hbricca@KCH-wxcenter-19.valpo.edu>
2018-08-25 15:58:18 -0500

    Temp conversion to K,C,F test case 1

--- temp_conversion.for
+++ temp_conversion.for
@@ -1,21 +1,23 @@
       PROGRAM temp_conversion
 
 **************************************************
-* Temperature conversion in Kelvin, Celcius, and
-* Fahrenheit
-*
-* By Heather Bricca
-*
-* August 25,2018
-*
-*
-* Modifications: (none)
-*
-* I have neither given or received, nor have I 
-* tolerated others' use of unauthorized aid.
-*    
-*
-*
+* Temperature conversion in Kelvin, Celcius, and *
+* Fahrenheit                                     *
+*                                                *
+* By Heather Bricca                              *
+*                                                *
+* August 25,2018                                 *
+*                                                *
+*                                                *
+* Modifications: (none)                          *
+*                                                *
+* Worked with Jilliann Dufort                    *
+*                                                *
+* I have neither given or received, nor have I   *
+* tolerated others use of unauthorized aid.      *
+*                                                *
+*                                                *
+*                                                *
 **************************************************
 
       real F, C, K
@@ -34,17 +36,6 @@
       PRINT *, 'Tc' , C
       PRINT *, 'Tf' , F
 
-*     Equation to convert from F to C
-*      tmpc = (tf-32)*(5.0/9.0)
-
-*      print *,tmpc
-*      print *, 'We should get -6.67C'
-
-*     Equation to convert from F to K
-*      tmpk = tmpc+273.15
- 
-*      print *,tmpk
-*      print *, 'We should 266.48K'
 
       END  
 
