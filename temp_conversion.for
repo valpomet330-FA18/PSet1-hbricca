5b945267c6e0384819c197e066e0a7eaeba0ea84
heather bricca <hbricca@KCH-wxcenter-19.valpo.edu>
2018-08-25 15:09:22 -0500

    My first commit for temp_conversion for Kelvin imput

--- temp_conversion.for
+++ temp_conversion.for
@@ -1,12 +1,43 @@
       PROGRAM temp_conversion
 
-*******************************************
-* Temperature Conversion Program
-* 
-* By: Heather Bricca
+**************************************************
+* Temperature conversion in Kelvin, Celcius, and
+* Fahrenheit
+*
+* By Heather Bricca
+*
+* August 25,2018
+*
+*
+* Modifications: (none)
 *
 * I have neither given or received, nor have I 
-* tolerated others' use of unauthorized aid. Heather Bricca
+* tolerated others' use of unauthorized aid.
+*    
 *
-***************************************************
+*
+**************************************************
+
+      real F, C, K
+      
+      PRINT *, 'What is the temperature in Kelvin?'
+      READ(*,*) K
+
+*     Equation to convert from K to C
+      C = K-273.15
+
+*     Equation to convert from F to C
+      tmpc = (tf-32)*(5.0/9.0)
+
+      print *,tmpc
+      print *, 'We should get -6.67C'
+
+*     Equation to convert from F to K
+      tmpk = tmpc+273.15
+ 
+      print *,tmpk
+      print *, 'We should 266.48K'
+
+      END  
+
 
